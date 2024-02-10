5039022f96979eab06d8bf4c4ad7c8079d0534af
joknarf <joknarf@free.fr>
2024-02-10 13:31:26 +0100

    noop test

--- manifests/test.pp
+++ manifests/test.pp
@@ -10,8 +10,10 @@ class deferlib::test()
     #ensure => Deferred('unless_file', ['/tmp/maintenance', 'running']),
     #ensure => Deferred('onlyif_file', ['/tmp/production', 'running']),
     #ensure => Deferred('onlyif_cmd', ['false', 'running']),
-    ensure => Deferred('agent_cmd', [ '[ -f /tmp/flag ] || echo running']),
-    #ensure => Deferred('agent_cmd', [ 'cat /tmp/cron_local_ensure', 'running']),
+    #ensure => Deferred('agent_cmd', ['[ -f /tmp/flag ] || echo running']),
+    ensure => 'running',
+    noop   => Deferred('unless_file', ['/tmp/flag', false, true]),
+    #ensure => Deferred('agent_cmd', ['cat /tmp/cron_local_ensure', 'running']),
   }
 
   
