2a5b15212238ab578ab1299dcfb14826f6ac7c49
joknarf <joknarf@free.fr>
2024-02-13 22:23:08 +0100

    test/sample update

--- manifests/test.pp
+++ manifests/test.pp
@@ -18,7 +18,7 @@ class deferlib::test {
   notify { 'script from module':
     message => deferlib::cmd({
         'command' => file("${module_name}/myscript"),
-        'match'   => 'hello',
+        'match'   => '^(hello|yop)$',
         'else'    => 'bad output',
     }),
   }
@@ -39,20 +39,12 @@ class deferlib::test {
     noop   => deferlib::if_file('/tmp/flag', true, false),
     #noop   =>deferlib::unless_file('/tmp/flag', false, true),
   }
-  # use deferred command output in env variable
-  exec { 'env var deferred':
-    command     => 'echo "$myvar"',
-    provider    => 'shell',
-    environment => [deferlib::cmd({
-          'command' => 'echo "myvar=$(cat /tmp/msg)"',
-    })],
-    logoutput   => true,
+
+  $def_val = deferlib::cmd({ 'command' => 'python3 --version' })
+  notify { 'test deferred concat string':
+    message => Deferred('sprintf', ['%s: %s', 'python package', $def_val]),
   }
 
-  $def_val = deferlib::cmd({ 'command' => 'cat /tmp/msg' })
-  notify { 'test string':
-    message => Deferred('sprintf', ['myvar=%s', $def_val]),
-  }
   notify { 'check':
     message => deferlib::if_file('/tmp/ok', 'file found', 'file not found'),
   }
